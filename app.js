cff82ed4341a4c5828f3dcc05f8eae12bebacfef
marcelo oliveira <marcelooliveira@mail.com>
2013-04-16 09:15:45 -0700

    update

--- app.js
+++ app.js
@@ -3,19 +3,53 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	http = require('http');
 
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+
+  var allowedOrigins = [
+    'http://localhost:9090',
+    'http://localhost:3000'
+  ];
+
+  var origin = req.headers.origin;
+
+  if(allowedOrigins.indexOf(origin) < 0) {
+    next();
+  }
+
+  res.header('Access-Control-Allow-Origin',  origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-Requested-With, X-File-Name, Content-Type, Content-Encoding, File-Type, Origin, X-Resource-Type, X-Resource-Id, X-Auth-Token');
+  res.header('Access-Control-Allow-Credentials', true);
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+
+  else {
+    next();
+  }
+};
+
+
 var app = express();
 
 // Configure: bodyParser to parse JSON data  
 // 	          methodOverride to implement custom HTTP methods  
 //            router to crete custom routes
 app.configure(function () {
+	app.use(allowCrossDomain);
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
+	
 });
 
 app.configure('development', function() {
 	app.use(express.errorHandler());
+	 app.use(allowCrossDomain);
 })
 
 // Sample routes are in a separate module, just for keep the code clean
