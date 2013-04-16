b7104134f4f215a0628eb9e533ac28d3d9234d3a
marcelo oliveira <marcelooliveira@mail.com>
2013-04-16 10:28:36 -0700

    update

--- app.js
+++ app.js
@@ -4,33 +4,18 @@ var express = require('express'),
 	http = require('http');
 
 
-//CORS middleware
 var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  var allowedOrigins = [
-    'http://localhost:9090',
-    'http://localhost:3000'
-  ];
-
-  var origin = req.headers.origin;
-
-  if(allowedOrigins.indexOf(origin) < 0) {
-    next();
-  }
-
-  res.header('Access-Control-Allow-Origin',  origin);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-Requested-With, X-File-Name, Content-Type, Content-Encoding, File-Type, Origin, X-Resource-Type, X-Resource-Id, X-Auth-Token');
-  res.header('Access-Control-Allow-Credentials', true);
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-
-  else {
-    next();
-  }
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
 };
 
 
@@ -44,12 +29,12 @@ app.configure(function () {
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
-	
 });
 
 app.configure('development', function() {
+	app.use(allowCrossDomain);
 	app.use(express.errorHandler());
-	 app.use(allowCrossDomain);
+	 
 })
 
 // Sample routes are in a separate module, just for keep the code clean
